bd9367c275fb20484bbe7779ebd3b87ec8ac9f41
Mustafa Emir Akay <emirakay073@gmail.com>
2021-03-25 21:44:07 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/navbar'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
+import { SidebarData } from './sidebar_data';
 import './navbar.css';
 import { IconContext } from 'react-icons';
 
