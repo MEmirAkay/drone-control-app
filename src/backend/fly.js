2368c84eaa15e0a0e60b71b86048c5259534ecad
Mustafa Emir Akay <emirakay073@gmail.com>
2021-03-26 15:17:15 +0300

    launch test done

---
+++ src/backend/fly.js
@@ -0,0 +1,42 @@
+const dgram = require('dgram');
+const wait = require('waait');
+const commandDelays = require('./commandDelays');
+
+const PORT = 8889;
+const HOST = '192.168.10.1';
+
+const drone = dgram.createSocket('udp4');
+drone.bind(PORT);
+
+drone.on('message', message => {
+    console.log(`🤖 : ${message}`);
+  });
+
+function handleError (err) {
+    if (err) {
+      console.log('ERROR')
+      console.log(err)
+    }
+  }
+
+  const commands = ['command', 'battery?', 'takeoff', 'land'];
+
+  let i = 0;
+
+async function go() {
+    const command = commands[i];
+    const delay = commandDelays[command];
+    console.log('running command : ${ command }');
+    drone.send(command, 0, command.length, PORT, HOST, handleError);
+
+    await wait(delay);
+    i += 1;
+
+    if (i < command.length) {
+        return go();
+    }
+    console.log('done!');
+
+  }
+
+go();
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-
-
 export class Home extends Component {
   static displayName = Home.name;
 
