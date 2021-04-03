cb5ae3d7dfe0143dcbf69ce0cc354cacaf6ec7e9
Mustafa Emir Akay <emirakay073@gmail.com>
2021-04-03 17:02:48 +0300

    commit

--- src/backend/fly.js
+++ src/backend/fly.js
@@ -1,10 +1,8 @@
 const dgram = require('dgram');
-const wait = require('waait');
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const throttle = require('lodash/throttle');
-const commandDelays = require('./commandDelays');
 
 const PORT = 8889;
 const HOST = '192.168.10.1';
@@ -37,7 +35,7 @@ function handleError (err) {
   }
 }
 
-
+drone.send('command', 0, 'command'.length, PORT, HOST, handleError);
 
 /*
 
@@ -68,13 +66,14 @@ go();
 */
 
 
-io.on('connection', (socket) => { 
-  socket.on('command',command => { // HTTP response
-    console.log('Commend sent from browser');
+io.on('connection', socket => {
+  socket.on('command', command => {
+    console.log('command Sent from browser');
+    console.log(command);
+    drone.send(command, 0, command.length, PORT, HOST, handleError);
   });
 
-  socket.emit('status', 'CONNECTED');//HTTP request
-
+  socket.emit('status', 'CONNECTED');
 });
 
 droneState.on(
